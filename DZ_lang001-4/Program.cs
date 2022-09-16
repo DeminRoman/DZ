82ef54ee696cea89c39f67aed86a67e1e781286f
DeminRoman <deminroman@mail.ru>
2022-09-16 15:07:27 +0300

    Задача 4

---
+++ DZ_lang001-4/Program.cs
@@ -0,0 +1,23 @@
+﻿// : Напишите программу, которая на вход принимает число (N), 
+// : а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+
+Console.Clear();
+
+Console.Write("Number: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int num = 1;
+
+while (num <= number)
+{
+    if (num % 2 != 1)
+    {
+    Console.Write(num + "  ");
+    }
+    num += 1;
+}
+
