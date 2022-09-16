57e6183f135ab00ecc00671a3fa3f4a6278352f9
DeminRoman <deminroman@mail.ru>
2022-09-16 14:26:29 +0300

    Задача 2

---
+++ DZ_Lang001-2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("Введите число A");
+int num_A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int num_B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C");
+int num_C = Convert.ToInt32(Console.ReadLine());
+
+int max = num_A;
+
+if (num_A > max) max = num_A;
+if (num_B > max) max = num_B;
+if (num_C > max) max = num_C;
+
+
+Console.Write("max =" + max);
