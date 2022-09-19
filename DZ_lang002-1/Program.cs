1112e45cf3ab41630056f19dd50295f812eb1da7
DeminRoman <deminroman@mail.ru>
2022-09-19 23:23:05 +0300

    DZ_lang002-1

---
+++ DZ_lang002-1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int amount = num.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    int num2 = ((num / 10) % 10);
+    Console.WriteLine($"Вторая цифра:{num2} ");
+}
