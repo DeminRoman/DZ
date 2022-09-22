6c91b0990a367c12509f37ec18e5a9fa9ec8f74e
DeminRoman <deminroman@mail.ru>
2022-09-22 23:29:18 +0300

    Задача 23

---
+++ DZ_lang003-3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+    Console.Write(Math.Pow(i, 3) + " ");
+
