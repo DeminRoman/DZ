e35268fe1ec93d6db8a20948bac2cb23adfec878
DeminRoman <deminroman@mail.ru>
2022-09-22 23:54:52 +0300

    Задача **

---
+++ DZ_lang003-4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает радиус круга и 
+// находит его площадь округленную до целого числа, необходимо вывести 
+// максимальную цифру в полученном округлённом значении площади круга
+
+
+Console.Clear();
+Console.WriteLine("Введите значение Радиуса:");
+double r = Convert.ToDouble(Console.ReadLine());
+
+double square = Math.PI * r * r;
+Console.WriteLine(" Площадь круга =, " + square, Math.Round(square, 0));
