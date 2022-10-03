9ac85513e5460ad813cb307520a52540c0e7d94c
DeminRoman <deminroman@mail.ru>
2022-10-03 22:23:01 +0300

    Задача 41

---
+++ DZ_lang006-1/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+Console.WriteLine("Введите несколько чисел через запятую:  ");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+Console.WriteLine();
+PrintArray(numbers);
+
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество значений больше 0 = {sum}");
+
+
+
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
