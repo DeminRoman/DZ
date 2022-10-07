b14b230e9162e5ac70b39e6187d8b791f2ef8661
DeminRoman <deminroman@mail.ru>
2022-10-07 18:47:00 +0300

    Задача 47

--- DZ_lang007-1/Program.cs
+++ DZ_lang007-1/Program.cs
@@ -9,10 +9,10 @@ int linesVol = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите количество столбцов");
 int columnsVol = Convert.ToInt32(Console.ReadLine());
 double[,] numbers = new double[linesVol, columnsVol];
-FillArrayRandomNumbers(numbers);
+FillArray(numbers);
 PrintArray(numbers);
 
-void FillArrayRandomNumbers(double[,] array)
+void FillArray(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
