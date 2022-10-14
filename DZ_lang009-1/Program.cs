0b1035e41f87373e3256ffbcd89c154526a43f65
DeminRoman <deminroman@mail.ru>
2022-10-14 14:35:22 +0300

    Задача 64

---
+++ DZ_lang009-1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 64: Задайте значения N и M. Напишите программу, 
+// которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5-> 2, 4
+// M = 4; N = 8-> 4, 6, 8
+
+Console.Clear();
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Чётные числа:");
+Even(N, M);
+Console.WriteLine();
+
+
+void Even(int N,int M)
+{
+    if (M < N)
+    {
+        return;
+    }
+
+    if (M % 2 == 0)
+        Even(N, M - 2);
+    else
+        Even(N, M - 1);
+
+    if (M % 2 == 0)
+    {
+        Console.Write(M + " ");
+    }
+}                 
